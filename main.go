28db9b2f89acfdea9bf8acb440b90fc5196a5763
Hari John Kuriakose <ejhari@gmail.com>
2023-03-10 18:50:51 +0530

    * Add workflow dispatch event * Add build-time flags

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import (
 	"github.com/mitchellh/colorstring"
 )
 
+// Build-time flags.
+var Version string
+var ShortCommit string
+
 func main() {
+	fmt.Printf("%s\n", Version)
+	fmt.Printf("%s\n", ShortCommit)
+
 	msg := colorstring.Color("[green]Hello [red]World!\n")
-	fmt.Printf(msg)
+	fmt.Printf("%s\n", msg)
 }
